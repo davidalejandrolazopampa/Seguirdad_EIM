a14f2adc0b67eac8b564cb8b596184b9a0ae0fda
osoman2 <osman.vilchez@utec.edu.pe>
2021-12-19 23:15:18 -0500

    end to end

---
+++ web/templates/crud_chat.js
@@ -0,0 +1,77 @@
+$(function(){
+    var url = "http://127.0.0.1:5000/chat";
+    var urlUsers = "http://127.0.0.1:5000/users";
+
+
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: url ,
+            insertUrl: url ,
+            updateUrl: url ,
+            deleteUrl: url ,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+        editing: {
+
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
+        remoteOperations: {
+            sorting: true,
+            paging: true
+        },
+        paging: {
+            pageSize: 12
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [8, 12, 20]
+        },
+        columns: [{
+            dataField: "id",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "content"
+        }, {
+            dataField: "sent_on",
+            dataType: "datetime",
+            allowEditing: false
+        }, {
+            dataField: "last_seen",
+            dataType: "datetime",
+            allowEditing: false
+        }, {
+            dataField: "user_from_id",
+            lookup: {
+                dataSource: DevExpress.data.AspNet.createStore({
+                    key: "id",
+                    loadUrl: urlUsers ,
+                    onBeforeSend: function(method, ajaxOptions) {
+                        ajaxOptions.xhrFields = { withCredentials: true };
+                    }
+                }),
+                displayExpr: "username",
+                valueExpr: "id"
+            }
+        }, {
+            dataField: "user_to_id",
+            lookup: {
+                dataSource: DevExpress.data.AspNet.createStore({
+                    key: "id",
+                    loadUrl: urlUsers,
+                    onBeforeSend: function(method, ajaxOptions) {
+                        ajaxOptions.xhrFields = { withCredentials: true };
+                    }
+                }),
+                displayExpr: "username",
+                valueExpr: "id"
+
+            }
+        },  ],
+    }).dxDataGrid("instance");
+});
